5a28bdab68764f9e1d7d9069cae2ffad5c3ee68f
kipermai <kiper_mail@i.ua>
2023-03-19 19:45:37 +0200

    My_first_JS

---
+++ fileee.js
@@ -0,0 +1,14 @@
+var a = 'Hellow';
+
+function b() {
+    if (false) {
+        var a = 'World'
+    }
+    else {
+        var b = 'man'
+    }
+    console.log(b);
+    console.log(a);
+}
+console.log(new Date());
+b()
